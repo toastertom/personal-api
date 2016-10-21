cd5305fce2c16d04d9899b75e94cd469ba9876c8
ToasterTom <tate.thomas5595@gmail.com>
2016-10-20 18:47:47 -0600

    added controller folder

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(middleware.addHeaders);
+
+var middleware = require('./controller/middleware.js')
+var mainCtrl = require('./controller/mainCtrl.js')
