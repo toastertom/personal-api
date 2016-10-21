ff53d78420439ae9fd093e23908cb0e0e4ba1b71
ToasterTom <tate.thomas5595@gmail.com>
2016-10-20 18:36:40 -0600

    first commit

---
+++ user.js
@@ -0,0 +1,54 @@
+var user = {
+  name: null,
+  location: null,
+  occupation: [],
+  hobbies: [
+    {
+      name: null,
+      type: null
+    },
+    {
+      name: null,
+      type: null
+    }
+    {
+      name: null,
+      type: null
+    },
+  ]
+  family: [
+    {
+      name: null,
+      relation: null,
+      gender: null
+    },
+    {
+      name: null,
+      relation: null,
+      gender: null
+    },
+    {
+      name: null,
+      relation: null,
+      gender: null
+    }
+  ]
+  resturants: [
+    {
+      name: null,
+      type: null,
+      rating: null
+    },
+    {
+      name: null,
+      type: null,
+      rating: null
+    },
+    {
+      name: null,
+      type: null,
+      rating: null
+    }
+  ]
+};
+module.exports = user;
